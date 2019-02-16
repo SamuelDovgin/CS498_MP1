8c0df961a31f98701103efcdc84710575e5a59bc
s3dovgin <s3dovgin@gmail.com>
2019-02-16 15:23:51 -0600

    small changes

--- source/scripts/main.js
+++ source/scripts/main.js
@@ -1,2 +1,9 @@
 /* Your JS here. */
 /*console.log('Hello World!')*/
+
+//var nameValue = document.getElementById("uniqueID").value;
+
+function ExampleJS(){
+    barValue = document.getElementById("barID").value;
+    alert("Your barcode is: " + barValue);
+ }
